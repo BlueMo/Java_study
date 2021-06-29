53abb1feffb4274fb295c1440f47281177686dd2
Jymoon <moon960323@gmail.com>
2021-06-29 13:55:04 +0900

    add interface example (basic)

---
+++ OOP/src/calc/Calc.java
@@ -0,0 +1,12 @@
+package calc;
+
+public interface Calc {
+	
+	double PI = 3.14159;
+	int ERROR = -99999999;
+	
+	int add(int num1, int num2);
+	int substract(int num1, int num2);
+	int times(int num1, int num2);
+	int divide(int num1, int num2);
+}
