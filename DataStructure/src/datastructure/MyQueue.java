3d770d332227134d7f4b3b6be542228e9fa097c5
Jymoon <moon960323@gmail.com>
2021-07-03 13:42:36 +0900

    add exception code

--- DataStructure/src/datastructure/MyQueue.java
+++ DataStructure/src/datastructure/MyQueue.java
@@ -6,15 +6,18 @@ public class MyQueue<T> extends MyLinkedList<T>{
 		insertElement(data, count);
 	}
 	
-	public T pop() {
+	public T pop() throws NullPointerException{
+		if (count == 0) throw new NullPointerException("Queue is empty.");
 		return removeElement(0).getData();
 	}
 	
 	public T front() {
+		if (count == 0) return null;
 		return head.getData();
 	}
 	
 	public T back() {
+		if (count == 0) return null;
 		return rear.getData();
 	}
 }
--- DataStructure/src/datastructure/MyStack.java
+++ DataStructure/src/datastructure/MyStack.java
@@ -6,11 +6,13 @@ public class MyStack<T> extends MyLinkedList<T>{
 		insertElement(data, count);
 	}
 	
-	public T pop() {
+	public T pop() throws NullPointerException{
+		if (count == 0) throw new NullPointerException("Stack is empty.");
 		return removeElement(count - 1).getData();
 	}
 	
 	public T top() {
+		if (count == 0) return null;
 		return rear.getData();
 	}
 }
