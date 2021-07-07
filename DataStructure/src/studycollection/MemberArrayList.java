59b57e9acc1f3038c6e0ce1fad87615035abc4e6
Jymoon <moon960323@gmail.com>
2021-07-07 16:47:03 +0900

    add ArrayList example code

---
+++ DataStructure/src/studycollection/MemberArrayList.java
@@ -0,0 +1,38 @@
+package studycollection;
+
+import java.util.*;
+
+public class MemberArrayList {
+	
+	private ArrayList<Member> memberList;
+	
+	public MemberArrayList() {
+		memberList = new ArrayList<>();
+	}
+	
+	public MemberArrayList(int initCapacity) {
+		memberList = new ArrayList<>(initCapacity);
+	}
+	
+	public void addMember(Member member) {
+		memberList.add(member);
+	}
+	
+	public boolean removeMember(int memberId) {
+		for (int i = 0; i < memberList.size(); i++) {
+			Member member = memberList.get(i);
+			
+			if (member.getMemberId() == memberId) {
+				memberList.remove(i);
+				return true;
+			}
+		}
+		
+		System.out.println(memberId + "가 존재하지 않습니다.");
+		return false;
+	}
+	
+	public void showAllMember() {
+		memberList.forEach(member -> System.out.println(member) );
+	}
+}
